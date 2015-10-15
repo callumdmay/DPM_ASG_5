aabe8d2560a23910c5126643e75b7e24b667bb57
Callum May <callumdmay@gmail.com>
2015-10-15 18:39:03 -0400

    UltrasonicPoller implements timer listener now
    -Timing works better with TimerListener

--- src/ev3WallFollower/UltrasonicPoller.java
+++ src/ev3WallFollower/UltrasonicPoller.java
@@ -1,38 +1,42 @@
 package ev3WallFollower;
 
-import ev3Navigator.Navigator;
 import lejos.robotics.SampleProvider;
+import lejos.utility.Timer;
+import lejos.utility.TimerListener;
 
 //
 //  Control of the wall follower is applied periodically by the 
 //  UltrasonicPoller thread.  The while loop at the bottom executes
 //  in a loop.  Assuming that the us.fetchSample, and cont.processUSData
 //  methods operate in about 20mS, and that the thread sleeps for
-//  50 mS at the end of each loop, then one cycle through the loop
-//  is approximately 70 mS.  This corresponds to a sampling rate
-//  of 1/70mS or about 14 Hz.
+//  10 mS at the end of each loop, then one cycle through the loop
+//  is approximately 30 mS.  This corresponds to a sampling rate
+//  of 1/70mS or about 33 Hz.
 //
 
 
-public class UltrasonicPoller extends Thread{
+public class UltrasonicPoller implements TimerListener{
 	private SampleProvider ultraSonicSensorSampleProvider;
 	private float[] usData;
-	private boolean isAvoidingObstacle;
 	//set to 30 as we need the distance variable to be initialized above the obstacle detection distance
 	private int distance = 30;
 	private Object lock;
+	private Timer lcdTimer;
+	public static final int LCD_REFRESH = 10;
 
 	public UltrasonicPoller(SampleProvider us, float[] usData) {
 		this.ultraSonicSensorSampleProvider = us;
 		this.usData = usData;
-		isAvoidingObstacle = false;
 		lock = new Object();
+		
+		this.lcdTimer = new Timer(LCD_REFRESH, this);
+		lcdTimer.start();
 	}
 
 	//  Sensors now return floats using a uniform protocol.
 	//  Need to convert US result to an integer [0,255]
 
-	public void run() {
+	public void timedOut() {
 		while (true) {
 			ultraSonicSensorSampleProvider.fetchSample(usData,0);							// acquire data
 
@@ -41,7 +45,6 @@ public class UltrasonicPoller extends Thread{
 				setDistance((int)(usData[0]*100.0));
 			}
 			
-			try { Thread.sleep(100); } catch(Exception e){}		// Poor man's timed sampling
 		}
 	}
 
